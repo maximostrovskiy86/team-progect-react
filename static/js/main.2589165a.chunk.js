bc3f5839c92e09a783b59639038fc8d24495cd6c
Maxim Ostrovsky <maxim.ostrovskiy86@gmail.com>
2021-10-26 11:27:05 +0300

    Updates

--- static/js/main.2589165a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-03-phonebook"]=this["webpackJsonpgoit-react-hw-03-phonebook"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(0)),a=function(){return Object(s.jsx)(s.Fragment,{})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.2589165a.chunk.js.map
---
+++ static/js/main.dc722044.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-phonebook"]=this["webpackJsonpgoit-react-hw-03-phonebook"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),h=(n(8),n(0)),s=function(){return Object(h.jsx)(h.Fragment,{children:Object(h.jsx)("h1",{children:"Hello!"})})};i.a.render(Object(h.jsx)(c.a.StrictMode,{children:Object(h.jsx)(s,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.dc722044.chunk.js.map
