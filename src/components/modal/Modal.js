d41a3ceadfd0c5d2fee64999e1532e9fd539864d
Halamai <halamaipawlo@gmail.com>
2021-10-28 10:27:58 +0300

    closeModale

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { useEffect } from "react";
 import { createPortal } from "react-dom";
-// import PropTypes from "prop-types";
 import s from "./Modal.module.scss";
 import sprite from "../../images/modal/close.svg";
 import spriter from "../../images/modal/sprite.svg";
 import { useMediaQuery } from "react-responsive";
 
 const modalRoot = document.querySelector("#modal-root");
-// const [onClose, setOnClose] = useState(false);
-// const toggleModal = () => setOnClose((prev) => !prev);
 
-const Modal = ({ onClose, children }) => {
+const Modal = ({ toggle, children }) => {
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
     return () => {
@@ -21,12 +18,12 @@ const Modal = ({ onClose, children }) => {
 
   const handleKeyDown = (event) => {
     if (event.code === "Escape") {
-      onClose();
+      toggle();
     }
   };
   const backdropClick = (event) => {
     if (event.currentTarget === event.target) {
-      onClose();
+      toggle();
     }
   };
 
@@ -37,19 +34,24 @@ const Modal = ({ onClose, children }) => {
     <div className={s.Overlay} onClick={backdropClick}>
       <div className={s.Modal}>
         {isTabletOrDesktop && (
-          <svg className={s.ModalCloseSvg} onClick={onClose}>
+          <svg className={s.ModalCloseSvg} onClick={toggle}>
             <use href={sprite + "#icon-close"} />
           </svg>
         )}
         {orMobile && (
           <div className={s.ModalExit}>
-            <svg className={s.ModalCloseSvgBack} onClick={onClose}>
+            <svg className={s.ModalCloseSvgBack} onClick={toggle}>
               <use href={spriter + "#exit"} />
             </svg>
           </div>
         )}
+        <ul>
+          <li>{children}</li>
+        </ul>
 
-        {children}
+        <button onClick={toggle} type="submit" className={s.button}>
+          Начать худеть
+        </button>
       </div>
     </div>,
     modalRoot
@@ -57,7 +59,3 @@ const Modal = ({ onClose, children }) => {
 };
 
 export default Modal;
-
-
-
-
--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -1,29 +1,29 @@
 import style from "./MainPage.module.css";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import DailyCaloriesForm from "../../components/dailyCaloriesForm/DailyCaloriesForm";
 import DailyCalorieIntake from "../../components/dailyCalorieIntake/DailyCalorieIntake";
 import Modal from "../../components/modal/Modal";
 
 export default function MainPage() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [result, setResult] = useState([]);
+  console.log(result);
 
-    const [isOpen, setIsOpen] = useState(false);
-    const [result, setResult] = useState([]);
-    console.log(result)
+  const toggle = () => setIsOpen((prev) => !prev);
+  const setValue = (value) => {
+    setResult(value);
+  };
 
-    const toggle = () => setIsOpen(prev => !prev);
-    const setValue = (value) => {
-        setResult(value);
-    }
-
-    return (
-        <>
-            <main className={style.main}>
-                <DailyCaloriesForm toggle={toggle} setValue={setValue}/>
-                {isOpen && <Modal>
-                    <DailyCalorieIntake result={result}/>
-                </Modal>}
-            </main>
-        </>
-    );
+  return (
+    <>
+      <main className={style.main}>
+        <DailyCaloriesForm toggle={toggle} setValue={setValue} />
+        {isOpen && (
+          <Modal toggle={toggle}>
+            <DailyCalorieIntake result={result} />
+          </Modal>
+        )}
+      </main>
+    </>
+  );
 }
-
