6a17c861b3334f2aa0c05da49285cef8b2d2fed9
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2021-10-28 15:59:25 +0300

    divided

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -3,7 +3,7 @@ import logoFull from "../../images/logoJPG.jpg";
 import logo from "../../images/logo-mob-JPG.jpg";
 const Logo = () => {
   return (
-    <img srcset={logo} alt="logo" width="47" height="44" />
+    <img srcSet={logo} alt="logo" width="47" height="44" />
 
     // <picture>
     //   <source srcset="../../images/logoJPG.jpg" media="(min-width:768px)" />
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,40 +1,48 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from "redux-persist";
 // import logger from "redux-logger";
 import storage from "redux-persist/lib/storage";
 // import { contactsReducer } from "./phonebook";
 import { authReducer } from "./authorization";
+import {
+  persistedAuthReducer,
+  persistedUserReducer,
+} from "./authorization/slice";
 
 const middleware = (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-    });
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  });
 // .concat(logger);
 
-const authPersistConfig = {
-    key: "auth",
-    storage,
-    // whitelist: ["user"],
-};
+// const authPersistConfig = {
+//   key: "auth",
+//   storage,
+// };
+
+// const userPersistConfig = {
+//   key: "user",
+//   storage,
+// };
 
 const store = configureStore({
-    reducer: {
-        auth: persistReducer(authPersistConfig, authReducer),
-        // contacts: contactsReducer,
-    },
-    middleware,
-    devTools: process.env.NODE_ENV === "development",
+  reducer: {
+    user: persistedUserReducer,
+    auth: persistedAuthReducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === "development",
 });
 
 const persistor = persistStore(store);
