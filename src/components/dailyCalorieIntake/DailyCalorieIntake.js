9af3ee79e1b30719c8a97def3b8181d8d0fe1136
IgorSukovsky <Sukovsky70@gmail.com>
2021-10-28 16:11:51 +0300

    added function-calculater-form

--- src/components/dailyCalorieIntake/DailyCalorieIntake.js
+++ src/components/dailyCalorieIntake/DailyCalorieIntake.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-const DailyCalorieIntake = ({result}) => {
-
-    return (
-        <>
-            <h2>Ваша рекомендуемая суточная норма калорий составляет</h2>
-            <p>2800 Ккал</p>
-            <ul>
-                {result.slice(0, 8).map(item => (<li key={item}>{item}</li>))}
-            </ul>
-        </>
-
-    );
-}
+const DailyCalorieIntake = ({ result }) => {
+  return (
+    <>
+      <h2>Ваша рекомендуемая суточная норма калорий составляет</h2>
+      <p>{result.dailyRate}Ккал</p>
+      <ul>
+        {result.notAllowedProducts?.slice(0, 8).map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default DailyCalorieIntake;
