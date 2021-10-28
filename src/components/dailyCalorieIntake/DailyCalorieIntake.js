60c26673c87dad1efd900dcafa86d95138e52720
Maxim Ostrovsky <maxim.ostrovskiy86@gmail.com>
2021-10-28 10:28:20 +0300

    1

--- src/components/dailyCalorieIntake/DailyCalorieIntake.js
+++ src/components/dailyCalorieIntake/DailyCalorieIntake.js
@@ -5,9 +5,10 @@ const DailyCalorieIntake = ({result}) => {
     return (
         <>
             <h2>Ваша рекомендуемая суточная норма калорий составляет</h2>
-            <p>2800 Ккал</p>
+            <p>2800</p>
             <ul>
                 {result.slice(0, 8).map(item => (<li key={item}>{item}</li>))}
+
             </ul>
         </>
 
