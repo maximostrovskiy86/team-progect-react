a9ad763fb02a8386e262840004e0a919187e59b8
Maxim Ostrovsky <maxim.ostrovskiy86@gmail.com>
2021-10-29 11:03:57 +0300

    1

--- src/redux/authorization/operations.js
+++ src/redux/authorization/operations.js
@@ -1,103 +1,139 @@
 import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import {createAsyncThunk} from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://slimmom-backend.goit.global";
 
 const token = {
-  set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  },
-  unset() {
-    axios.defaults.headers.common.Authorization = "";
-  },
+    set(token) {
+        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    },
+    unset() {
+        axios.defaults.headers.common.Authorization = "";
+    },
 };
 
 const register = createAsyncThunk(
-  "auth/register",
-  async ({ username, email, password }, { rejectWithValue }) => {
-    try {
-      await axios.post("/auth/register", {
-        username,
-        email,
-        password,
-      });
-      const { data } = await axios.post("/auth/login", {
-        email,
-        password,
-      });
-      token.set(data.accessToken);
-      return data;
-    } catch (error) {
-      return rejectWithValue(error.message);
+    "auth/register",
+    async ({username, email, password}, {rejectWithValue}) => {
+        try {
+            await axios.post("/auth/register", {
+                username,
+                email,
+                password,
+            });
+            const {data} = await axios.post("/auth/login", {
+                email,
+                password,
+            });
+            token.set(data.accessToken);
+            return data;
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
     }
-  }
 );
 
 const logIn = createAsyncThunk(
-  "auth/login",
-  async (credentials, { rejectWithValue }) => {
-    try {
-      const { data } = await axios.post("/auth/login", credentials);
-      token.set(data.accessToken);
-      return data;
-    } catch (error) {
-      return rejectWithValue(error.message);
+    "auth/login",
+    async (credentials, {rejectWithValue}) => {
+        try {
+            const {data} = await axios.post("/auth/login", credentials);
+            token.set(data.accessToken);
+            return data;
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
     }
-  }
 );
 
 const logOut = createAsyncThunk(
-  "auth/logout",
-  async (credentials, { rejectWithValue }) => {
-    try {
-      await axios.post("/auth/logout", credentials);
-      token.unset();
-    } catch (error) {
-      return rejectWithValue(error.message);
+    "auth/logout",
+    async (credentials, {rejectWithValue}) => {
+        try {
+            await axios.post("/auth/logout", credentials);
+            token.unset();
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
     }
-  }
 );
 
 const refreshAccessToken = createAsyncThunk(
-  "auth/refresh",
-  async (_, { rejectWithValue, getState }) => {
-    const state = getState();
-    const refreshToken = state.auth.refreshToken;
-    const sid = state.auth.sid;
+    "auth/refresh",
+    async (_, {rejectWithValue, getState}) => {
+        const state = getState();
+        const refreshToken = state.auth.refreshToken;
+        const sid = state.auth.sid;
 
-    if (refreshToken === null) {
-      return rejectWithValue();
+        if (refreshToken === null) {
+            return rejectWithValue();
+        }
+        token.set(refreshToken);
+        try {
+            const {data} = await axios.post("/auth/refresh", {sid});
+            return data;
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
     }
-    token.set(refreshToken);
-    try {
-      const { data } = await axios.post("/auth/refresh", { sid });
-      return data;
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
 );
 
 const refreshUserData = createAsyncThunk(
-  "/user/refresh",
-  async (_, { rejectWithValue, getState }) => {
-    const state = getState();
-    const accessToken = state.auth.accessToken;
-    token.set(accessToken);
-    try {
-      const { data } = await axios.get("/user");
-      return data;
-    } catch (error) {
-      return rejectWithValue(error.message);
+    "/user/refresh",
+    async (_, {rejectWithValue, getState}) => {
+        const state = getState();
+        const accessToken = state.auth.accessToken;
+        token.set(accessToken);
+        try {
+            const {data} = await axios.get("/user");
+            return data;
+        } catch (error) {
+            return rejectWithValue(error.message);
+        }
     }
-  }
 );
 
+// const rateDailyUse = createAsyncThunk(
+//     "/user/rateDaily",
+//     async (_, {rejectWithValue, getState}) => {
+//         const state = getState();
+//         const id = state.user.id
+//         console.log(state)
+//         const accessToken = state.auth.accessToken;
+//         token.set(accessToken);
+//
+//         try {
+//             const {data} = await axios.get(`/daily-rate/${id}`);
+//             return data;
+//         } catch (error) {
+//             return rejectWithValue(error.message);
+//         }
+//     }
+// );
+
+// export const rateDailyUser = (values) => async (dispatch, getState) => {
+//     dispatch(updateCalculatorRequest());
+//     try {
+//         const data = transformString(values);
+//         const userId = getUserId(getState());
+//         const state = getState();
+//         const accessToken = state.auth.accessToken;
+//         token.set(accessToken);
+//         const res = await axios.post(`${BASE_URL}/daily-rate/${userId}`, data);
+//
+//         dispatch(updateCalculatorSuccess(res.data));
+//     } catch (error) {
+//         dispatch(updateCalculatorError());
+//     }
+// };
+
+
 const operations = {
-  register,
-  logOut,
-  logIn,
-  refreshAccessToken,
-  refreshUserData,
+    register,
+    logOut,
+    logIn,
+    refreshAccessToken,
+    refreshUserData,
+    // rateDailyUse,
 };
 export default operations;
