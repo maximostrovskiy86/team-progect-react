96b84a7c122ad8aa5be708db769dc0c7f9418317
Evgeniy-the-Great <shalenkoevgeniy@gmail.com>
2021-10-29 15:57:17 +0300

    add

---
+++ src/components/diaryAddProductForm/DiaryAddProductForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+import style from "./DiaryAddProductForm.module.scss";
+const DiaryAddProductForm = () => {
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form className={style.diaryProductForm} onSubmit={onHandleSubmit}>
+      <label>
+        <input
+          className={style.productInput}
+          type="text"
+          placeholder="Введите название продукта"
+          list="produsts"
+        />
+      </label>
+      <datalist id="produsts">
+        <option value="Chocolate" />
+        <option value="Coconut" />
+        <option value="Mint" />
+        <option value="Strawberry" />
+        <option value="Vanilla" />
+      </datalist>
+      {/* <datalist>
+        {products.map((product) => (
+          <option value=""></option>
+        ))}
+      </datalist> */}
+      <label>
+        <input className={style.weightInput} type="text" placeholder="Граммы" />
+      </label>
+      <button type="submit" className={style.diaryProductFormBtn}>
+        +
+      </button>
+    </form>
+  );
+};
+
+export default DiaryAddProductForm;
--- src/pages/diaryPage/DiaryPage.js
+++ src/pages/diaryPage/DiaryPage.js
@@ -1,10 +1,15 @@
+import DiaryAddProductForm from "../../components/diaryAddProductForm/DiaryAddProductForm";
+import DiaryDateСalendar from "../../components/diaryDateСalendar/DiaryDateСalendar";
 import RightSideBar from "../../components/rightSideBar/RightSideBar";
 import style from "./DiaryPage.module.scss";
 
 export default function DiaryPage() {
   return (
     <div className={style.diaryPageWraper}>
-      <h2> DiaryPage</h2>
+      <div>
+        <DiaryDateСalendar />
+        <DiaryAddProductForm />
+      </div>
       <RightSideBar />
     </div>
   );
