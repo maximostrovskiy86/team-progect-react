b6795c160086f49fa3c0d21e696d9e53a25286cd
Evgeniy-the-Great <shalenkoevgeniy@gmail.com>
2021-10-30 11:59:15 +0300

    add calendar styles

--- src/components/diaryDateСalendar/DiaryDateСalendar.js
+++ src/components/diaryDateСalendar/DiaryDateСalendar.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import DatePicker from "react-datepicker";
+import { ReactComponent as CalendarLogo } from "../../images/calendar.svg";
 import "react-datepicker/dist/react-datepicker.css";
+import styles from "./DiaryDateСalendar.module.scss";
 // import {dailyOperations} from "../../redux/daily";
 // import {useDispatch} from "react-redux";
 
-const DiaryDateСalendar = ({selectDate}) => {
+const DiaryDateСalendar = ({ selectDate }) => {
   const [startDate, setStartDate] = useState(new Date());
   const customeDate = moment(startDate).format("YYYY-MM-DD");
   selectDate(customeDate);
@@ -13,17 +15,21 @@ const DiaryDateСalendar = ({selectDate}) => {
   // const dispatch = useDispatch();
   console.log(customeDate);
 
-    // useEffect(() => {
-    //     dispatch(dailyOperations.fetchDayInfo({date: customeDate}))
-    // }, [startDate]);
-
+  // useEffect(() => {
+  //     dispatch(dailyOperations.fetchDayInfo({date: customeDate}))
+  // }, [startDate]);
 
   return (
-    <div>
-      <DatePicker
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-      />
+    <div className={styles.сalendar}>
+      <label>
+        <DatePicker
+          className={styles.datePicker}
+          selected={startDate}
+          dateFormat="dd.MM.yyyy"
+          onChange={(date) => setStartDate(date)}
+        />
+        <CalendarLogo className={styles.datePickerLogo} />
+      </label>
     </div>
   );
 };
