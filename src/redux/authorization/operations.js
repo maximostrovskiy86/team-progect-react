e4e0462d06aeff6c33e9801fca8d136a54a1c378
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2021-11-03 20:55:02 +0200

    token refresh logic

--- src/redux/authorization/operations.js
+++ src/redux/authorization/operations.js
@@ -1,96 +1,102 @@
 import axios from "axios";
-import {createAsyncThunk} from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://slimmom-backend.goit.global";
 
 const token = {
-    set(token) {
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-    },
-    unset() {
-        axios.defaults.headers.common.Authorization = "";
-    },
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = "";
+  },
 };
 
 const register = createAsyncThunk(
-    "auth/register",
-    async ({username, email, password}, {rejectWithValue}) => {
-        try {
-            await axios.post("/auth/register", {
-                username,
-                email,
-                password,
-            });
-            const {data} = await axios.post("/auth/login", {
-                email,
-                password,
-            });
-            token.set(data.accessToken);
-            return data;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+  "auth/register",
+  async ({ username, email, password }, { rejectWithValue }) => {
+    try {
+      await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      const { data } = await axios.post("/auth/login", {
+        email,
+        password,
+      });
+      token.set(data.accessToken);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
+  }
 );
 
 const logIn = createAsyncThunk(
-    "auth/login",
-    async (credentials, {rejectWithValue}) => {
-        try {
-            const {data} = await axios.post("/auth/login", credentials);
-            token.set(data.accessToken);
-            return data;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+  "auth/login",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post("/auth/login", credentials);
+      token.set(data.accessToken);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
+  }
 );
 
 const logOut = createAsyncThunk(
-    "auth/logout",
-    async (credentials, {rejectWithValue}) => {
-        try {
-            await axios.post("/auth/logout", credentials);
-            token.unset();
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+  "auth/logout",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      await axios.post("/auth/logout", credentials);
+      token.unset();
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
+  }
 );
 
 const refreshAccessToken = createAsyncThunk(
-    "auth/refresh",
-    async (_, {rejectWithValue, getState}) => {
-        const state = getState();
-        const refreshToken = state.auth.refreshToken;
-        const sid = state.auth.sid;
+  "auth/refresh",
+  async (_, { rejectWithValue, getState }) => {
+    const state = getState();
+    const refreshToken = state.auth.refreshToken;
+    const sid = state.auth.sid;
 
-        if (refreshToken === null) {
-            return rejectWithValue();
-        }
-        token.set(refreshToken);
-        try {
-            const {data} = await axios.post("/auth/refresh", {sid});
-            return data;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+    if (refreshToken === null) {
+      return rejectWithValue();
     }
+    token.set(refreshToken);
+    try {
+      const { data } = await axios.post("/auth/refresh", { sid });
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
 );
 
 const refreshUserData = createAsyncThunk(
-    "/user/refresh",
-    async (_, {rejectWithValue, getState}) => {
-        const state = getState();
-        const accessToken = state.auth.accessToken;
-        token.set(accessToken);
-        try {
-            const {data} = await axios.get("/user");
-            return data;
-        } catch (error) {
-            return rejectWithValue(error.message);
-        }
+  "/user/refresh",
+  async (_, { rejectWithValue, getState, dispatch }) => {
+    const state = getState();
+    const accessToken = state.auth.accessToken;
+    token.set(accessToken);
+
+    try {
+      const { data } = await axios.get("/user");
+      return data;
+    } catch (error) {
+      const status = error.response.status;
+      if (status === "401") {
+        dispatch(refreshAccessToken());
+      } else {
+        rejectWithValue(error.message);
+      }
     }
+  }
 );
 
 // export const rateDailyUser = (values) => async (dispatch, getState) => {
@@ -109,12 +115,11 @@ const refreshUserData = createAsyncThunk(
 //     }
 // };
 
-
 const operations = {
-    register,
-    logOut,
-    logIn,
-    refreshAccessToken,
-    refreshUserData,
+  register,
+  logOut,
+  logIn,
+  refreshAccessToken,
+  refreshUserData,
 };
 export default operations;
